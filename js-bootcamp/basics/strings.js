01f3d9791248989f092eb5b5a4c0be06175be688
Andrej Skeledzija <andrej.skeledzija@gmail.com>
2019-11-23 12:34:24 +0100

    strings

--- js-bootcamp/basics/strings.js
+++ js-bootcamp/basics/strings.js
@@ -3,3 +3,8 @@ let lastName = 'Skeledzija'
 let fullName = firstName + ' ' + lastName
 
 console.log(fullName)
+
+let city = 'Osijek'
+let country = 'Croatia'
+let location = city + ', ' + country
+console.log(location)
