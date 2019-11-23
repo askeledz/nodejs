0542646a6e6925a5fffb6b4764f523442b6f96d9
Andrej Skeledzija <andrej.skeledzija@gmail.com>
2019-11-23 13:20:28 +0100

    tem converter

---
+++ js-bootcamp/basics/temp-coversion.js
@@ -0,0 +1,20 @@
+
+// Fahrenheit coverter
+let fahrenheit = 32
+
+// convert values
+// C = (F - 32) / 1.8
+// K = (F + 459.67) * 5/9
+// print that values
+
+
+let C = (fahrenheit - 32) / 1.8
+let K = (fahrenheit + 459.67) * 5 / 9
+let F = (1.8 * C) + 32
+
+console.log('Fahrenheti: ' + F)
+console.log('Kelvin: ' + K)
+console.log('Celsius: ' + C)
+
+
+
---
+++ js-bootcamp/basics/variables.js
@@ -0,0 +1,15 @@
+// 1. you can't define variable more then once
+let petName = 'Hal'
+petName = 'Spot'
+
+console.log(petName)
+
+// 2. There are rules related to variable names (only letters, underscore and dollar sign --> _az$) ...can't start with number
+
+//let 3 = 2
+let result = 3 + 4
+
+// 3. Variale names can't be reserved keywords
+
+// let let = 12 // is not valid
+
