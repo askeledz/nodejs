d93bce1273362c8ea5d42deeb1175b632d6ba391
Andrej Skeledzija <andrej.skeledzija@gmail.com>
2019-11-23 13:22:29 +0100

    Minor typo issue

--- js-bootcamp/basics/temp-coversion.js
+++ js-bootcamp/basics/temp-coversion.js
@@ -12,7 +12,7 @@ let C = (fahrenheit - 32) / 1.8
 let K = (fahrenheit + 459.67) * 5 / 9
 let F = (1.8 * C) + 32
 
-console.log('Fahrenheti: ' + F)
+console.log('Fahrenheit: ' + F)
 console.log('Kelvin: ' + K)
 console.log('Celsius: ' + C)
 
